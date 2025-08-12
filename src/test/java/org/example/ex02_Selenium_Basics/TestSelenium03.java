52b6a2ca8a93f02681e8cc2334f4d1ac59741cc0
Prateek Sharma <sharmaprateek24@gmail.com>
2025-08-12 06:28:34 +0530

    Selenium-12-8-25

--- src/test/java/org/example/ex02_Selenium_Basics/TestSelenium03.java
+++ src/test/java/org/example/ex02_Selenium_Basics/TestSelenium03.java
@@ -1,4 +1,23 @@
 package org.example.ex02_Selenium_Basics;
 
+import io.qameta.allure.Description;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 public class TestSelenium03 {
+    @Description("Open the app.vwo.com and get the title")
+    @Test
+    public void test_Selenium01()
+    {
+        //Selenium3
+        //You need to setup the driver(browser)
+        //System.getProperty("webdriver.geckodriver","pathgeckodriver");
+
+        //Selenium4
+        //Selenium Manager -utility -which can download the driver automatically
+        //Start and Stop itself.
+        ChromeDriver driver =new ChromeDriver();
+        driver.get("https://google.com");
+    }
 }
