d2cfaeb143995d7b3d981aaeb8af982eecf7dd04
Prateek Sharma <sharmaprateek24@gmail.com>
2025-08-27 22:37:51 +0530

    Selenium-27-8-25-Handling SVG-phase-2

--- src/test/java/org/example/ex08_SVG/TestSelenium30_SVG_P1.java
+++ src/test/java/org/example/ex08_SVG/TestSelenium30_SVG_P1.java
@@ -33,9 +33,14 @@ public class TestSelenium30_SVG_P1 {
         WebElement search_bar = driver.findElement(By.xpath("//input[@title='Search for Products, Brands and More']"));
         search_bar.sendKeys("macmini");
 
-        //Step-2>Click on teh SVG element
+        //Step-2>Click on the SVG element
         List<WebElement> svgElements =driver.findElements(By.xpath("//*[name()='svg']"));
         svgElements.get(0).click();
+        List<WebElement> title_Result =driver.findElements(By.xpath("//div[contains(@data-id,'CPU')]/div/a[2]"));
+       for (WebElement title : title_Result)
+       {
+           System.out.println(title.getText());
+       }
     }
     @AfterTest
     public void closeBrowser()
---
+++ src/test/java/org/example/ex08_SVG/TestSelenium31_SVG_P2.java
@@ -0,0 +1,52 @@
+package org.example.ex08_SVG;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TestSelenium31_SVG_P2 {
+    ChromeDriver driver;
+    @BeforeTest
+    public void Open_Browser()
+    {
+        ChromeOptions chromeOptions =new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        driver =new ChromeDriver(chromeOptions);
+    }
+    @Test
+    @Description("Verify that tripura is in India and click on it")
+
+    public void test_svg_india_search_click()
+    {
+        driver.get("https://www.amcharts.com/svg-maps/?map=india");
+        List<WebElement> states = driver.findElements(By.xpath("//*[local-name()='svg']/*[local-name()='g'][7]/*[local-name()='g']/*[local-name()='g']/*[local-name()='path']"));
+     for (WebElement state : states)
+     {
+         System.out.println(state.getAttribute("aria-label"));
+         if(state.getAttribute("aria-label").contains("Tripura"))
+         {
+             state.click();
+         }
+     }
+
+    }
+    @AfterTest
+    public void Close_Browser()
+    {
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+
+
+        }
+        driver.quit();
+    }
+}
