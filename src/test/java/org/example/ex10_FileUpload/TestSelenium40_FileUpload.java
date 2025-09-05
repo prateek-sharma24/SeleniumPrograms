3e5df80b6bd86e60907cad0004d3a665630aaf77
Prateek Sharma <sharmaprateek24@gmail.com>
2025-09-05 06:40:01 +0530

    Selenium-5-9-25-File upload-fix

--- src/test/java/org/example/ex10_FileUpload/TestSelenium40_FileUpload.java
+++ src/test/java/org/example/ex10_FileUpload/TestSelenium40_FileUpload.java
@@ -11,5 +11,14 @@ public class TestSelenium40_FileUpload extends CommonToAll {
     {
         driver.get("https://awesomeqa.com/selenium/upload.html");
         WebElement file_upload =driver.findElement(By.id("fileToUpload"));
+        String working_dir = System.getProperty("user.dir");
+        //the full path if given will work on my machine not on other machines
+        //C:\Users\shiv\IdeaProjects\SeleniumPrograms
+        String path_file =working_dir+"/src/test/java/org/example/Dat.txt";
+        file_upload.sendKeys(path_file);
+        WebElement upload_button =driver.findElement(By.name("submit"));
+        upload_button.click();
+
+
     }
 }
